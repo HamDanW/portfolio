355118c65bc659430333b01e4399296127c77dbd
Ryan Wong <rcwong@ucdavis.edu>
2023-05-17 14:54:11 -0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,15 @@ import { Link } from "gatsby"
 import Pdf from '../pages/Resume.pdf'
 
 import "./header-styling.css"
-import ProjectURl from '../pages/projects'
-import PlaygroundURL from '../pages/my-playground'
-import AboutMeURL from '../pages/about-me'
 
 const Header = ({ siteTitle }) => (
   <header>
     <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href={ProjectURl}>Projects</a></li>
-        <li><a href={PlaygroundURL}>My Playground</a></li>
-        <li><a href={AboutMeURL}>About Me</a></li>
+        <Link to="/">Home</Link>
+        <Link to="../pages/projects">Projects</Link>
+        <Link to="../pages/my-playground">My Playground</Link>
+        <Link to="../pages/about-me">About Me</Link>
         <li><a href={Pdf} target="_blank" rel="noopener noreferrer">Resume</a></li>
       </ul>
     </nav>

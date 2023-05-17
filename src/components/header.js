0186e6da427f0540b85f150b8a2e12c82002e4b2
Ryan Wong <rcwong@ucdavis.edu>
2023-05-17 14:37:18 -0700

    fixed header urls

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,18 @@ import { Link } from "gatsby"
 import Pdf from '../pages/Resume.pdf'
 
 import "./header-styling.css"
+import ProjectURl from '../pages/projects'
+import PlaygroundURL from '../pages/my-playground'
+import AboutMeURL from '../pages/about-me'
 
 const Header = ({ siteTitle }) => (
   <header>
     <nav>
       <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="../pages/projects">Projects</a></li>
-        <li><a href="../pages/my-playground">My Playground</a></li>
-        <li><a href="../pages/about-me">About Me</a></li>
+        <li><a href={ProjectURl}>Projects</a></li>
+        <li><a href={PlaygroundURL}>My Playground</a></li>
+        <li><a href={AboutMeURL}>About Me</a></li>
         <li><a href={Pdf} target="_blank" rel="noopener noreferrer">Resume</a></li>
       </ul>
     </nav>

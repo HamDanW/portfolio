adf126bc3fcac037b8f382d3a146da636385bd6e
Ryan Wong <rcwong@ucdavis.edu>
2023-05-31 15:02:05 -0700

    Update simple-shell.js

--- src/pages/projects/simple-shell.js
+++ src/pages/projects/simple-shell.js
@@ -7,8 +7,9 @@ import Seo from "../../components/seo"
 const SimpleShell = () => (
   <Layout>
     <h1>Simple Shell</h1>
+    <hr/>
     <h2>Introduction </h2>
-    <hr></hr>
+    <p>A shell is a command-line interface, also known as a CLI. Once a command is entered, the shell interprets it and communicates with the operating system to execute the requested actions. It acts as an intermediary between the user and the operating system, facilitating the execution of various tasks and managing system resources. </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

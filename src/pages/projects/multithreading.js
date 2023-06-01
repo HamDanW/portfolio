0115da95e61155aeaefab8bbb3b30c682f3bd0a5
Ryan Wong <rcwong@ucdavis.edu>
2023-06-01 14:42:03 -0700

    update project page

---
+++ src/pages/projects/multithreading.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+const Multithreading = () => (
+  <Layout>
+    <h1>Still under construction</h1>
+    <h1>Multi-threading Library</h1>
+    <hr/>
+    <h2>Introduction </h2>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export const Head = () => <Seo title="Simple-Shell" />
+
+export default Multithreading

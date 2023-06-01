c4895ac019feaabd3967ad641891b2d5a1cc406a
Ryan Wong <rcwong@ucdavis.edu>
2023-06-01 00:46:40 -0700

    fixing project page

---
+++ src/pages/projects/blockchain.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+const Blockchain = () => (
+  <Layout>
+    <h1>Still under construction</h1>
+    <h1>BlockChain Project</h1>
+    <hr/>
+    <h2>Introduction </h2>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export const Head = () => <Seo title="Simple-Shell" />
+
+export default Blockchain

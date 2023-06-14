c95824d1653225091d25dd1ae8e7fa9736e2fe3d
Ryan Wong <rcwong@ucdavis.edu>
2023-06-13 23:00:01 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,49 +1,41 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import "./footer-styling.css"
-import "dark-mode-styling.css"
+import "./dark-mode-styling.css"
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Footer = () => (
     <footer>
-        <div class="wrapper">
-            <div class="candles">
-                <div class="light__wave"></div>
-                <div class="candle1">
-                    <div class="candle1__body">
-                        <div class="candle1__eyes">
-                            <span class="candle1__eyes-one"></span>
-                            <span class="candle1__eyes-two"></span>
+        <div className="wrapper">
+            <div className="candles">
+                <div className="light__wave"></div>
+                <div className="candle1">
+                    <div className="candle1__body">
+                        <div className="candle1__eyes">
+                            <span className="candle1__eyes-one"></span>
+                            <span className="candle1__eyes-two"></span>
                         </div>
-                        <div class="candle1__mouth"></div>
+                        <div className="candle1__mouth"></div>
                     </div>
-                    <div class="candle1__stick"></div>
+                    <div className="candle1__stick"></div>
                 </div>
 
-                <div class="candle2">
-                    <div class="candle2__body">
-                        <div class="candle2__eyes">
-                            <div class="candle2__eyes-one"></div>
-                            <div class="candle2__eyes-two"></div>
+                <div className="candle2">
+                    <div className="candle2__body">
+                        <div className="candle2__eyes">
+                            <div className="candle2__eyes-one"></div>
+                            <div className="candle2__eyes-two"></div>
                         </div>
                     </div>
-                    <div class="candle2__stick"></div>
+                    <div className="candle2__stick"></div>
                 </div>
-                <div class="candle2__fire"></div>
-                <div class="sparkles-one"></div>
-                <div class="sparkles-two"></div>
-                <div class="candle__smoke-one">
-
-                </div>
-                <div class="candle__smoke-two">
-
-                </div>
-
+                <div className="candle2__fire"></div>
+                <div className="sparkles-one"></div>
+                <div className="sparkles-two"></div>
+                <div className="candle__smoke-one"></div>
+                <div className="candle__smoke-two"></div>
             </div>
-            <div class="floor">
-            </div>
-
-
+            <div className="floor"></div>
         </div>
         <div class="social-icons">
             <a href="http://Linkedin.com/in/ryan-c-wong" target="_blank" class="icon linkedin">

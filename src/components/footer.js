643067c521cb68f098b25e5616c4b6ff6614db63
Ryan Wong <rcwong@ucdavis.edu>
2023-06-17 16:37:57 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,68 +1,53 @@
 import React, { useState } from 'react';
 import { Link } from "gatsby"
 import "./footer-styling.css"
-//import "./dark-mode-styling.css"
-import "./dark-mode-test.css"
+import "./dark-mode-styling.css"
+//import "./dark-mode-test.css"
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Footer = () => {
-  // const [isLightMode, setIsLightMode] = useState(true);
+  const [animationActive, setAnimationActive] = useState(false);
 
-  // const toggleMode = () => {
-  //   setIsLightMode(!isLightMode);
-  // };
+  const toggleAnimation = () => {
+    setAnimationActive(!animationActive);
+  };
 
   return (
-    <footer /*className={isLightMode ? 'light' : 'dark'}*/>
-      <div className='wrapper'>
-        <div className="candles">
-          <div className="light__wave"></div>
-          <div className="candle1">
-            <div className="candle1__body">
-              <div className="candle1__eyes">
-                <span className="candle1__eyes-one"></span>
-                <span className="candle1__eyes-two"></span>
-              </div>
-              <div className="candle1__mouth"></div>
+    <div className="wrapper">
+      <div className={`candles ${animationActive ? 'animate' : ''}`}>
+        <div className="light__wave"></div>
+        <div className="candle1">
+          <div className="candle1__body">
+            <div className="candle1__eyes">
+              <span className="candle1__eyes-one"></span>
+              <span className="candle1__eyes-two"></span>
             </div>
-            <div className="candle1__stick"></div>
+            <div className="candle1__mouth"></div>
           </div>
-
-          <div className="candle2">
-            <div className="candle2__body">
-              <div className="candle2__eyes">
-                <div className="candle2__eyes-one"></div>
-                <div className="candle2__eyes-two"></div>
-              </div>
-            </div>
-            <div className="candle2__stick"></div>
-          </div>
-          <div className="candle2__fire"></div>
-          <div className="sparkles-one"></div>
-          <div className="sparkles-two"></div>
-          <div className="candle__smoke-one"></div>
-          <div className="candle__smoke-two"></div>
+          <div className="candle1__stick"></div>
         </div>
-        <div className="floor"></div>
-        {/* <button className='Dark-Mode-Button' onClick={toggleMode}>
-          {isLightMode ? 'Dark Mode' : 'Light Mode'}
-        </button> */}
+
+        <div className="candle2">
+          <div className="candle2__body">
+            <div className="candle2__eyes">
+              <div className="candle2__eyes-one"></div>
+              <div className="candle2__eyes-two"></div>
+            </div>
+          </div>
+          <div className="candle2__stick"></div>
+        </div>
+        <div className="candle2__fire"></div>
+        <div className="sparkles-one"></div>
+        <div className="sparkles-two"></div>
+        <div className="candle__smoke-one"></div>
+        <div className="candle__smoke-two"></div>
       </div>
-      <div className="social-icons">
-        <a href="http://Linkedin.com/in/ryan-c-wong" target="_blank" rel="noopener noreferrer" className="icon linkedin">
-          <span><i className="fab fa-linkedin"></i></span>
-        </a>
-        <a href="https://github.com/HamDanW" target="_blank" rel="noopener noreferrer" className="icon github">
-          <span><i className="fab fa-github"></i></span>
-        </a>
-      </div>
-      <div className="contact-info">
-        <p className="phone">Phone: (415)-606-3369</p>
-        <p> | </p>
-        <p className="email">Email: ryccwong@email.com</p>
-      </div>
-      <p>&copy; 2023 My Portfolio. All rights reserved.</p>
-    </footer>
+      <div className="floor"></div>
+
+      <button onClick={toggleAnimation}>
+        {animationActive ? 'Stop Animation' : 'Start Animation'}
+      </button>
+    </div>
   );
 };
 

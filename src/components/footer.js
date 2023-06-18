3c8ed792f8b42ffcfb052b9165b211b27e78ff35
Ryan Wong <rcwong@ucdavis.edu>
2023-06-17 17:05:07 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,42 +11,44 @@ const Footer = () => {
   const toggleAnimation = () => {
     setIsPlaying(!isPlaying);
   };
-  
-  return (
-    <div className="wrapper">
-      <div className={`candles ${isPlaying ? 'playing' : ''}`}>
-        <div className="light__wave"></div>
-        <div className="candle1">
-          <div className="candle1__body">
-            <div className="candle1__eyes">
-              <span className="candle1__eyes-one"></span>
-              <span className="candle1__eyes-two"></span>
-            </div>
-            <div className="candle1__mouth"></div>
-          </div>
-          <div className="candle1__stick"></div>
-        </div>
 
-        <div className="candle2">
-          <div className="candle2__body">
-            <div className="candle2__eyes">
-              <div className="candle2__eyes-one"></div>
-              <div className="candle2__eyes-two"></div>
+  return (
+    <footer>
+      <div className="wrapper">
+        <div className={`candles ${isPlaying ? 'playing' : ''}`}>
+          <div className="light__wave"></div>
+          <div className="candle1">
+            <div className="candle1__body">
+              <div className="candle1__eyes">
+                <span className="candle1__eyes-one"></span>
+                <span className="candle1__eyes-two"></span>
+              </div>
+              <div className="candle1__mouth"></div>
             </div>
+            <div className="candle1__stick"></div>
           </div>
-          <div className="candle2__stick"></div>
+
+          <div className="candle2">
+            <div className="candle2__body">
+              <div className="candle2__eyes">
+                <div className="candle2__eyes-one"></div>
+                <div className="candle2__eyes-two"></div>
+              </div>
+            </div>
+            <div className="candle2__stick"></div>
+          </div>
+          <div className="candle2__fire"></div>
+          <div className="sparkles-one"></div>
+          <div className="sparkles-two"></div>
+          <div className="candle__smoke-one"></div>
+          <div className="candle__smoke-two"></div>
         </div>
-        <div className="candle2__fire"></div>
-        <div className="sparkles-one"></div>
-        <div className="sparkles-two"></div>
-        <div className="candle__smoke-one"></div>
-        <div className="candle__smoke-two"></div>
+        <div className="floor"></div>
+        <button onClick={toggleAnimation}>
+          {isPlaying ? 'Pause Animation' : 'Play Animation'}
+        </button>
       </div>
-      <div className="floor"></div>
-      <button onClick={toggleAnimation}>
-        {isPlaying ? 'Pause Animation' : 'Play Animation'}
-      </button>
-    </div>
+    </footer>
   );
 };
 

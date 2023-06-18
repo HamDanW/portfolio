cbfb11fd30b61e27628983a98370924885570319
Ryan Wong <rcwong@ucdavis.edu>
2023-06-17 17:08:21 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -48,6 +48,21 @@ const Footer = () => {
           {isPlaying ? 'Pause Animation' : 'Play Animation'}
         </button>
       </div>
+      <div className="social-icons">
+        <a href="http://Linkedin.com/in/ryan-c-wong" target="_blank" rel="noopener noreferrer" className="icon linkedin">
+          <span><i className="fab fa-linkedin"></i></span>
+        </a>
+        <a href="https://github.com/HamDanW" target="_blank" rel="noopener noreferrer" className="icon github">
+          <span><i className="fab fa-github"></i></span>
+        </a>
+      </div>
+      <div className="contact-info">
+        <p className="phone">Phone: (415)-606-3369</p>
+        <p> | </p>
+        <p className="email">Email: ryccwong@email.com</p>
+      </div>
+      <p>&copy; 2023 My Portfolio. All rights reserved.</p>
+
     </footer>
   );
 };

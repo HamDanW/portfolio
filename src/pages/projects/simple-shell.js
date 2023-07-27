2e5c3fbfc1c5739f8a1cdc530676dd730fe4c6ec
Ryan Wong <rcwong@ucdavis.edu>
2023-07-26 17:17:01 -0700

    Update simple-shell.js

--- src/pages/projects/simple-shell.js
+++ src/pages/projects/simple-shell.js
@@ -4,9 +4,26 @@ import ReactMarkdown from "react-markdown"
 
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
-import mdContent from "simple-shell.md"
 
 const SimpleShell = () => {
+  const [mdContent, setMdContent] = useState("");
+
+  useEffect(() => {
+    const fetchMdContent = async () => {
+      try {
+        const response = await fetch("./simple-shell.md");
+        if (!response.ok) {
+          throw new Error("Failed to fetch markdown content.");
+        }
+        const text = await response.text();
+        setMdContent(text);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMdContent();
+  }, []);
+
   return (
     <Layout>
       {/* Display the converted markdown content as HTML */}

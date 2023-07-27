50c224b1a5491c679f671371b78887c83404e617
Ryan Wong <rcwong@ucdavis.edu>
2023-07-27 16:38:15 -0700

    no post found

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,12 +7,46 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+// exports.createPages = async ({ actions }) => {
+//   const { createPage } = actions
+//   createPage({
+//     path: "/using-dsg",
+//     component: require.resolve("./src/templates/using-dsg.js"),
+//     context: {},
+//     defer: true,
+//   })
+// }
+
+const path = require("path")
+
+exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  const blogPostTemplate = path.resolve(`src/templates/post.js`)
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+ `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: `/blog-post/`,
+        component:`/var/www/prokdoc/src/templates/blog-post.js`,
+        context: {},
+         })
+    })
   })
 }
--- src/pages/projects/simple-shell.js
+++ src/pages/projects/simple-shell.js
@@ -6,16 +6,7 @@ import Seo from "../../components/seo"
 export default function BlogPostTemplate({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  if (!markdownRemark) {
-    return (
-      <Layout>
-        <div>
-          <h1>Post Not Found</h1>
-        </div>
-      </Layout>
-    )
-  }
+  const { markdownRemark } = data.markdownRemark // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>

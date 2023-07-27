85ae89ff17ac32b949d4deaccae5d9b8a60ff710
Ryan Wong <rcwong@ucdavis.edu>
2023-07-26 17:00:59 -0700

    Update simple-shell.js

--- src/pages/projects/simple-shell.js
+++ src/pages/projects/simple-shell.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown from "react-markdown"
 
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
